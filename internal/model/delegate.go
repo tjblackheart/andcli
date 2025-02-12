627459b3c73ca07b105f710f70519d7886249198
tjblackheart <tjblackheart@users.noreply.github.com>
2025-02-12 12:55:48 +0100

    remove long if

--- internal/model/delegate.go
+++ internal/model/delegate.go
@@ -21,38 +21,41 @@ func (d itemDelegate) Render(w io.Writer, m list.Model, idx int, i list.Item) {
 	entry, _ := i.(vaults.Entry)
 	text := d.style.listItem.Render(entry.Title())
 
-	if idx == m.Index() {
-		token, exp := entry.GenerateTOTP()
-		until := exp - time.Now().Unix()
-		state.currentOTP = token
-
-		bgColor, fgColor := green, white
-		if until <= 10 && until > 5 {
-			bgColor, fgColor = yellow, black
-		}
-
-		if until <= 5 {
-			bgColor = red
-		}
-
-		formatted := "*** ***"
-		if state.showToken {
-			formatted = fmt.Sprintf("%s %s", state.currentOTP[:3], state.currentOTP[3:])
-		}
-
-		item := d.style.activeItem.Render(entry.Title())
-		if state.showUsernames {
-			user := style.username.Render(fmt.Sprintf("(%s) ", entry.Description()))
-			item = fmt.Sprintf("%s%s", item, user)
-		}
-
-		text = fmt.Sprintf(
-			"%s%s %s",
-			item,
-			style.token.Background(bgColor).Foreground(fgColor).Render(formatted),
-			style.until.Foreground(bgColor).Render(fmt.Sprintf("%vs", until)),
-		)
+	if idx != m.Index() {
+		fmt.Fprint(w, text)
+		return
 	}
+
+	token, exp := entry.GenerateTOTP()
+	until := exp - time.Now().Unix()
+	state.currentOTP = token
+
+	bgColor, fgColor := green, white
+	if until <= 10 && until > 5 {
+		bgColor, fgColor = yellow, black
+	}
+
+	if until <= 5 {
+		bgColor = red
+	}
+
+	formatted := "*** ***"
+	if state.showToken {
+		formatted = fmt.Sprintf("%s %s", state.currentOTP[:3], state.currentOTP[3:])
+	}
+
+	item := d.style.activeItem.Render(entry.Title())
+	if state.showUsernames {
+		user := style.username.Render(fmt.Sprintf("(%s) ", entry.Description()))
+		item = fmt.Sprintf("%s%s", item, user)
+	}
+
+	text = fmt.Sprintf(
+		"%s%s %s",
+		item,
+		style.token.Background(bgColor).Foreground(fgColor).Render(formatted),
+		style.until.Foreground(bgColor).Render(fmt.Sprintf("%vs", until)),
+	)
 
 	fmt.Fprint(w, text)
 }
