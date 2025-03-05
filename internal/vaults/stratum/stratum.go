8acb129257a2aae0c838ae5cd067e700d7264164
tjblackheart <tjblackheart@users.noreply.github.com>
2025-03-05 07:48:33 +0100

    add base

--- internal/vaults/stratum/stratum.go
+++ internal/vaults/stratum/stratum.go
@@ -1,5 +1,6 @@
 package stratum
 
+// https://github.com/stratumauth/app/blob/master/doc/BACKUP_FORMAT.md
 // https://github.com/stratumauth/app/blob/master/extra/decrypt_backup.py
 
 import (
@@ -11,10 +12,30 @@ import (
 // force interface impl
 var _ vaults.Vault = &vault{}
 
+const (
+	KEY_LENGTH = 32
+
+	// Default
+	HEADER      = "AUTHENTICATORPRO"
+	SALT_LENGTH = 16
+	IV_LENGTH   = 12
+	PARALLELISM = 4
+	ITERATIONS  = 3
+	MEMORY_SIZE = 65536
+
+	// Legacy
+	LEGACY_HEADER      = "AuthenticatorPro"
+	LEGACY_HASH_MODE   = "sha1"
+	LEGACY_ITERATIONS  = 64000
+	LEGACY_SALT_LENGTH = 20
+	LEGACY_IV_LENGTH   = 16
+)
+
 type vault struct {
 }
 
 func Open(filename string, pass []byte) (vaults.Vault, error) {
+
 	return nil, errors.New("not implemented")
 }
 
@@ -22,4 +43,12 @@ func (v vault) Entries() []vaults.Entry {
 	list := make([]vaults.Entry, 0)
 
 	return list
+}
+
+func (v vault) decrypt() {
+
+}
+
+func (v vault) decryptLegacy() {
+
 }
